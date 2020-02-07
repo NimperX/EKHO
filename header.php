84954de4e5711f93f3ca68b75f17ebc0f829ffaf
NimperX <nimnaperera98@gmail.com>
2020-02-07 19:44:13 +0530

    Completed homw page

--- header.php
+++ header.php
@@ -7,12 +7,13 @@
     </title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css" />
-    <link rel="favicon" href="img/ekho-main-logo.svg" />
+    <link rel="icon" href="img/ekho-main-logo.svg" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg fixed-top navbar-dark " style="background:#00000077">
-        <a class="navbar-brand" href="home.php"><img src="img/ekho-main-logo.svg" width="150" height="auto" class="d-inline-block align-top" alt=""/></a>
+        <a class="navbar-brand" href="home.php"><img src="img/ekho-main-logo.svg" width="150" height="auto" class="d-inline-block align-top" alt="" /></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -23,7 +24,7 @@
                     <a class="nav-link" href="home.php">Home</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Services
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -35,6 +36,9 @@
                 </li>
                 <li class="nav-item active">
                     <a class="nav-link" href="about.php">About us</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="gallery.php">Gallery</a>
                 </li>
                 <li class="nav-item active">
                     <a class="nav-link" href="contact.php">Contact us</a>
--- home.php
+++ home.php
@@ -6,7 +6,7 @@ include_once('header.php');
     <div class="row">
         <div class="col">
             <div class="top-content">
-                <h3>Your Guide To EKHO Safari, Tissamaharama</h3>
+                <h2>Your Guide To EKHO Safari, Tissamaharama</h2>
                 <p>A rejuvenating waterfront retreat - EKHO Safari occupies a prime position in Tissamaharama. Scenically located by the ancient “Tissa Wewa” providing undisturbed views of the lake.</p>
 
                 <p>The Tissamaharama property traces it's roots back when it started operation in 1967, now restored to a comfortable and beautifully laid lake view hotel.</p>
@@ -17,6 +17,64 @@ include_once('header.php');
                 </div>
             </div>
         </div>
+    </div>
+    <div class="row mt-5">
+        <div class="col text-center">
+            <h2>
+                Our Services
+            </h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 text-center mt-5">
+            <div class="circle">
+                <i class="material-icons">
+                    pool
+                </i>
+            </div>
+            <h3 class="mt-3">
+                Day-to-day Services
+            </h3>
+            <p class="mt-3 pt-3 pr-5 pl-5 text-justify">If you crave for few refreshing hours everyday in the water, we are renowned and many more...</p>
+            <div class="mr-5 ml-5">
+                <a href="daytoday.php">
+                    <button class="btn btn-block btn-outline-gold">Explore</button>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-4 text-center mt-5">
+            <div class="circle">
+                <i class="material-icons">
+                    meeting_room
+                </i>
+            </div>
+            <h3 class="mt-3">
+                Accomodation
+            </h3>
+            <p class="mt-3 pt-3 pr-5 pl-5 text-justify">Modern comforts and beautiful views of Tissamaharama await. Accommodation of the most indulgent kind awaits you at our Superior Rooms. After a day of exploring – or even just idling, it is a perfect refuge to return to a long shower.</p>
+            <div class="mr-5 ml-5">
+                <a href="room.php">
+                    <button class="btn btn-block btn-outline-gold">Checkout</button>
+                </a>
+            </div>
+        </div>
+         <div class="col-md-4 text-center mt-5">
+            <div class="circle">
+                <i class="material-icons">
+                    event
+                </i>
+            </div>
+            <h3 class="mt-3">
+                Events
+            </h3>
+            <p class="mt-3 pt-3 pr-5 pl-5 text-justify">Weddings, meetings, conferences, workshops, seminars, reunions, birthday parties, private parties, cocktail parties – the reasons to get together are endless and so are your options</p>
+            <div class="mr-5 ml-5">
+                <a href="hall.php">
+                    <button class="btn btn-block btn-outline-gold">Book now</button>
+                </a>
+            </div>
+        </div>
+        
     </div>
 </div>
 
