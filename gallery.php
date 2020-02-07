2d8aff786e7d4ef8e092a58d6bd31688b0b2d7ae
NimperX <nimnaperera98@gmail.com>
2020-02-08 01:53:01 +0530

    Gallery page completed

--- gallery.php
+++ gallery.php
@@ -1,7 +1,63 @@
 <?php
     include_once('header.php');
 ?>
-
+    <div class="container"> 
+        <div class="gallery-header text-center mt-5 pt-5">
+            <h2>Gallery</h2>
+        </div>
+        <div class="row mt-3 pl-3">
+            <h5>Public places</h5>
+        </div>
+        <div class="row mt-2 mb-5">
+            <?php
+            for($i=1;$i<7;$i++){
+                echo '
+                    <div class="col-md-3">    
+                        <img src="img/Gallery-Public-Spaces-'.$i.'.jpg" class="rounded img-thumbnail" />
+                    </div>';
+            }
+            ?>
+        </div>
+        <div class="row mt-3 pl-3">
+            <h5>Facilities</h5>
+        </div>
+        <div class="row mt-2 mb-5">
+            <?php
+            for($i=1;$i<8;$i++){
+                echo '
+                    <div class="col-md-3">    
+                        <img src="img/Gallery-Facilities-'.$i.'.jpg" class="rounded img-thumbnail" />
+                    </div>';
+            }
+            ?>
+        </div>
+        <div class="row mt-3 pl-3">
+            <h5>Rooms</h5>
+        </div>
+        <div class="row mt-2 mb-5">
+            <?php
+            for($i=1;$i<8;$i++){
+                echo '
+                    <div class="col-md-3">    
+                        <img src="img/Gallery-Rooms-'.$i.'.jpg" class="rounded img-thumbnail" />
+                    </div>';
+            }
+            ?>
+        </div>
+        <div class="row mt-3 pl-3">
+            <h5>Dining</h5>
+        </div>
+        <div class="row mt-2 mb-5">
+            <?php
+            for($i=1;$i<8;$i++){
+                echo '
+                    <div class="col-md-3">    
+                        <img src="img/Gallery-Dining-'.$i.'.jpg" class="rounded img-thumbnail" />
+                    </div>';
+            }
+            ?>
+        </div>
+    </div>
 <?php 
     include_once('footer.php');
 ?>
