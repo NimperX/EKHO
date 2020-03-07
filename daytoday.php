260a6c43ebcb430e70ee9b15b739b5d927cb2ea9
NimperX <nimnaperera98@gmail.com>
2020-03-07 21:23:46 +0530

    Finalized project

--- daytoday.php
+++ daytoday.php
@@ -1,7 +1,54 @@
 <?php
     include_once('src/header.php');
 ?>
-
+<div class="container mt-5 pt-5">
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1>Day-to-day Services</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <img src="img/pool.jpg" class="img-thumbnail" />
+            <h3>Pool</h3>
+            <p>Rs. 300.00(per person)</p>
+        </div>
+        <div class="col-md-4">
+            <img src="img/lunch-buffet.jpg" class="img-thumbnail" />
+            <h3>Lunch buffet</h3>
+            <p>Rs. 1200.00(per plate)</p>
+        </div>
+        <div class="col-md-4">
+            <img src="img/lunch-buffet.jpg" class="img-thumbnail" />
+            <h3>Dinner buffet</h3>
+            <p>Rs. 1000.00(per plate)</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <img src="img/bite-plate.jpg" class="img-thumbnail" />
+            <h3>Bite plate</h3>
+            <p>Rs. 600.00</p>
+        </div>
+        <div class="col-md-4">
+            <img src="img/special-dinner-table.jpg" class="img-thumbnail" />
+            <h3>Special Dinner Table</h3>
+            <p>Rs. 3000.00</p>
+        </div>
+        <div class="col-md-4">
+            <img src="img/cockage.jpg" class="img-thumbnail" />
+            <h3>Cockage</h3>
+            <p>Rs. 400.00</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <img src="img/ice-bucket.jpg" class="img-thumbnail" />
+            <h3>Ice bucket</h3>
+            <p>Rs. 300.00</p>
+        </div>
+    </div>
+</div>
 <?php 
     include_once('src/footer.php');
 ?>
