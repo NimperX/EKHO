2657f7fa89dcc7cc59e4a95d050b2d2f5b7354b8
NimperX <nimnaperera98@gmail.com>
2020-04-07 19:54:25 +0530

    Edit content

--- about.php
+++ about.php
@@ -1,7 +1,27 @@
 <?php
     include_once('src/header.php');
 ?>
+<div class="container"> 
+        <div class="gallery-header text-center mt-5 pt-5">
+            <h2>About us</h2>
+            <div class="row">
+                <div class="col-md-6 text-justify">
+                <p>Welcome to EKHO Safari Tissa Hotel, your number one source for relaxation. We're dedicated to giving you the very best of experience, with a focus on your preferences.
+Founded in 2000, EKHO Safari Tissa Hotel has come a long way from its beginnings. We now serve customers all over Sri Lanka, and are thrilled to be a part of the eco-friendly wing of the hotel industry.</p><br>
+<p>We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don't hesitate to contact us.</p><br>
+<p><a href="contact.php">Conatct us</a> for ant inquiry.</p>
+                </div>
+                <div class="col-md-6">
+                    <img src="img/Gallery-Facilities-3.jpg" alt="EKHO Safari Tissa Hotel" class="img-fluid"/>
+                </div>
+            </div>
+            
 
+
+        </div>
+
+        
+</div>
 <?php 
     include_once('src/footer.php');
 ?>
--- gallery.php
+++ gallery.php
@@ -9,53 +9,125 @@
             <h5>Public places</h5>
         </div>
         <div class="row mt-2 mb-5">
-            <?php
-            for($i=1;$i<7;$i++){
-                echo '
-                    <div class="col-md-3">    
-                        <img src="img/Gallery-Public-Spaces-'.$i.'.jpg" class="rounded img-thumbnail" />
-                    </div>';
-            }
-            ?>
+            <div class="col-md-6">
+                <img src="img/Gallery-Public-Spaces-1.jpg" class="rounded img-thumbnail" />
+            </div>
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-6 mb-2">
+                        <img src="img/Gallery-Public-Spaces-2.jpg" class="rounded img-thumbnail" />
+                    </div>
+                    <div class="col-md-6 mb-2">
+                        <img src="img/Gallery-Public-Spaces-3.jpg" class="rounded img-thumbnail" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 mb-2">
+                        <img src="img/Gallery-Public-Spaces-4.jpg" class="rounded img-thumbnail" />
+                    </div>
+                    <div class="col-md-6 mb-2">
+                        <img src="img/Gallery-Public-Spaces-5.jpg" class="rounded img-thumbnail" />
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="row mt-3 pl-3">
             <h5>Facilities</h5>
         </div>
         <div class="row mt-2 mb-5">
-            <?php
-            for($i=1;$i<8;$i++){
-                echo '
-                    <div class="col-md-3">    
-                        <img src="img/Gallery-Facilities-'.$i.'.jpg" class="rounded img-thumbnail" />
-                    </div>';
-            }
-            ?>
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-6 mb-2">
+                        <img src="img/Gallery-Facilities-2.jpg" class="rounded img-thumbnail" />
+                    </div>
+                    <div class="col-md-6 mb-2">
+                        <img src="img/Gallery-Facilities-3.jpg" class="rounded img-thumbnail" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4 mb-2">
+                        <img src="img/Gallery-Facilities-4.jpg" class="rounded img-thumbnail" />
+                    </div>
+                    <div class="col-md-4 mb-2">
+                        <img src="img/Gallery-Facilities-5.jpg" class="rounded img-thumbnail" />
+                    </div>
+                    <div class="col-md-4 mb-2">
+                        <img src="img/Gallery-Facilities-6.jpg" class="rounded img-thumbnail" />
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <img src="img/Gallery-Facilities-1.jpg" class="rounded img-thumbnail" />
+            </div>
         </div>
         <div class="row mt-3 pl-3">
             <h5>Rooms</h5>
         </div>
         <div class="row mt-2 mb-5">
-            <?php
-            for($i=1;$i<8;$i++){
-                echo '
-                    <div class="col-md-3">    
-                        <img src="img/Gallery-Rooms-'.$i.'.jpg" class="rounded img-thumbnail" />
-                    </div>';
-            }
-            ?>
+        <div class="row mt-2 mb-5">
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-12 mb-12">
+                        <img src="img/Gallery-Rooms-1.jpg" class="rounded img-thumbnail" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 mb-2">
+                        <img src="img/Gallery-Rooms-6.jpg" class="rounded img-thumbnail" />
+                    </div>
+                    <div class="col-md-6 mb-2">
+                        <img src="img/Gallery-Rooms-7.jpg" class="rounded img-thumbnail" />
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-6 mb-2">
+                        <img src="img/Gallery-Rooms-2.jpg" class="rounded img-thumbnail" />
+                    </div>
+                    <div class="col-md-6 mb-2">
+                        <img src="img/Gallery-Rooms-3.jpg" class="rounded img-thumbnail" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 mb-2">
+                        <img src="img/Gallery-Rooms-4.jpg" class="rounded img-thumbnail" />
+                    </div>
+                </div>
+                
+            </div>
         </div>
         <div class="row mt-3 pl-3">
             <h5>Dining</h5>
         </div>
         <div class="row mt-2 mb-5">
-            <?php
-            for($i=1;$i<8;$i++){
-                echo '
-                    <div class="col-md-3">    
-                        <img src="img/Gallery-Dining-'.$i.'.jpg" class="rounded img-thumbnail" />
-                    </div>';
-            }
-            ?>
+            <div class="col-md-7">
+                <div class="row">
+                    <div class="col-md-4 mb-2">
+                        <img src="img/Gallery-Dining-1.jpg" class="rounded img-thumbnail" />
+                    </div>
+                    <div class="col-md-4 mb-2">
+                        <img src="img/Gallery-Dining-2.jpg" class="rounded img-thumbnail" />
+                    </div>
+                    <div class="col-md-4 mb-2">
+                        <img src="img/Gallery-Dining-3.jpg" class="rounded img-thumbnail" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4 mb-2">
+                        <img src="img/Gallery-Dining-4.jpg" class="rounded img-thumbnail" />
+                    </div>
+                    <div class="col-md-4 mb-2">
+                        <img src="img/Gallery-Dining-5.jpg" class="rounded img-thumbnail" />
+                    </div>
+                    <div class="col-md-4 mb-2">
+                        <img src="img/Gallery-Dining-6.jpg" class="rounded img-thumbnail" />
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <img src="img/Gallery-Dining-7.jpg" class="rounded img-thumbnail" />
+            </div>
         </div>
     </div>
 <?php 
